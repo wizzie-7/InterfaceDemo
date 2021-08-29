0a0cfb1f6304b9a09c5c5b6dd9c83552365adf99
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 19:53:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            wildAnimal w;
+            InterfaceImple imp = new InterfaceImple();
+            imp.taste();
+            imp.color();
+        }
+    }
+    public interface wildAnimal
+    {
+        void taste();
+    }
+    public interface petAnimal 
+    {
+        void color();
+    }
+    public class InterfaceImple : wildAnimal,petAnimal
+    {
+        public void taste()
+        {
+            Console.WriteLine("Senssing diff taste");
+        }
+        public void color()
+        {
+            Console.WriteLine("White");
+        }
+    }
+    abstract class interfcaAbstractclass : petAnimal
+    {
+        public void color()
+        {
+            Console.WriteLine("colorful");
+        }
+    }
+}
